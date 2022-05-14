ceb3ad267e8974e625ff3d5bcf8c4489c01a99fc
Perlasof <94577878+Perlasof@users.noreply.github.com>
2022-05-14 18:34:35 -0300

    Proyecto final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AptitudesComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
